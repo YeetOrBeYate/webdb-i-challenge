98c275097a926a1f423cf83a275509835198d5f1
Kyle Corbin <kylecorbin26@gmail.com>
2019-12-09 17:26:44 -0500

    Got the queries done

--- queries.sql
+++ queries.sql
@@ -1,17 +1,56 @@
 -- Database Queries
 
+Tablenames	Records
+Customers	91
+Categories	8
+Employees	10
+OrderDetails	518
+Orders	196
+Products	77
+Shippers	3
+Suppliers	29
+
 -- Find all customers with postal code 1010
+
+select * 
+from Customers
+where PostalCode = "1010"
 
 -- Find the phone number for the supplier with the id 11
 
+select Phone 
+from Suppliers 
+where SupplierID = 11
+
 -- List first 10 orders placed, sorted descending by the order date
+
+select * from Orders
+Order by OrderDate desc 
+limit 10
 
 -- Find all customers that live in London, Madrid, or Brazil
 
+select * from Customers
+where Country = "Brazil"
+or City = "Madrid"
+or City = "London"
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+insert into Customers (CustomerName,ContactName,Address,City,PostalCode,Country)
+values("Bilbo Baggins", "The Shire", "1 Hobbit-Hole", "Bag End", "111", "Middle Earth")
+
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customers
+SET PostalCode = "11122"
+WHERE CustomerID = "92";
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+SELECT COUNT ( DISTINCT City ) 
+FROM Customers;
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
+SELECT * from Suppliers 
+where length(SupplierName) > 20
